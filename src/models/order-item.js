2586f52eb5e6c73b590567050770e240aed85d54
ferchox920 <fernandoramones92@gmail.com>
2023-08-08 00:34:02 -0300

    commit: define orders module

---
+++ src/models/order-item.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+})
+
+
+const OrderItem = mongoose.model('OrderItem', orderItemSchema);
+
+export default OrderItem;
